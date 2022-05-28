983b579c52bdb4c7960c509a67e5005bb912f561
Sergio Espeja <sergio.espeja@gmail.com>
2022-05-28 23:52:49 +0200

    Fix bad 'main' merge

--- main.js
+++ main.js
@@ -33,4 +33,18 @@ function main() {
 	}
 }
 
+function startConversion(path) {
+	vlog(1, `Starting conversion`);
+	const output = convertDirectory(path);
+
+	vlog(1,
+		`Fixed in ${output.elapsed}ms
+${'-'.repeat(8)}
+Directories: ${output.directories.length}
+Files: ${output.files.length}
+Markdown Links: ${output.markdownLinks}
+CSV Links: ${output.csvLinks}`
+	);
+}
+
 main();
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { vlog, flags } = require("./logger.js");
 
 const argHelp = [
